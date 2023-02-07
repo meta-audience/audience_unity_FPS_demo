b5b41de71f5d71b0e06c95ada9ef2dd716d48551
JonnyTsai <Jonny_Tsai@trendmicro.com>
2023-02-07 12:06:19 +0800

    PublishhToPublic

---
+++ Assets/_audienceDemo/Scripts/FPSCameraFollow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AudienceSDK.Sample
+{
+    public class FPSCameraFollow : CameraMoveAlgorithmBase
+    {
+        [SerializeField]
+        private Transform _followTarget = null;
+
+        [SerializeField]
+        private Vector3 _absOffset = Vector3.zero;
+
+        protected override void Start()
+        {
+            base.Start();
+            if (this._followTarget)
+            {
+                this.MoveCameras(Quaternion.identity, this._followTarget.position + this._absOffset, true);
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (this._followTarget)
+            {
+                this.MoveCameras(Quaternion.identity, this._followTarget.position + this._absOffset, true);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (this._followTarget)
+            {
+                this.MoveCameras(Quaternion.identity, this._followTarget.position + this._absOffset, true);
+            }
+        }
+    }
+}
